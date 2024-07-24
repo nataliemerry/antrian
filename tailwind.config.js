1f05c76382b6b3cef26fd15f85ee03def8fa28d4
222112243 <222112243@stis.ac.id>
2024-07-24 08:18:03 +0700

    set fontsize

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,21 @@ export default {
         error: '#E53835',
         success: '#2E7D31',
       },
+      fontSize: {
+        'xs': '.75rem',     // ukuran ekstra kecil
+        'sm': '.875rem',    // ukuran kecil
+        'base': '1rem',     // ukuran dasar (biasanya 16px)
+        'lg': '1.125rem',   // ukuran besar
+        'xl': '1.25rem',    // ukuran sangat besar
+        '2xl': '1.5rem',    // ukuran sangat besar
+        '3xl': '1.875rem',  // ukuran sangat besar
+        '4xl': '2.25rem',   // ukuran sangat besar
+        '5xl': '3rem',      // ukuran sangat besar
+        '6xl': '4rem',      // ukuran sangat besar
+        '7xl': '5rem',      // ukuran sangat besar
+        '8xl': '6rem',      // ukuran sangat besar
+        '9xl': '8rem',      // ukuran sangat besar
+      },
     },
   },
   plugins: [],
