53980e9c2626921b6c7df147fe2733eff604a8c8
222112243 <222112243@stis.ac.id>
2024-07-24 08:14:43 +0700

    set warna

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,19 @@ export default {
     "./resources/**/*.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        birubps: '#043277',
+        birunavbar: '#174A7C',
+        biru: '#0093DD',
+        orange: '#EB891B',
+        hijau: '#68B92E',
+        silver: '#F5F7FA',
+        white: '#FFFFFF',
+        error: '#E53835',
+        success: '#2E7D31',,
+      },
+    },
   },
   plugins: [],
 }
